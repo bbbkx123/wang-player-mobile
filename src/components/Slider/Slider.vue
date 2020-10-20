d307efbec4b4bbb82f31bd3623621e010a0ecb66
bbbkx <33077574+bbbkx123@users.noreply.github.com>
2020-10-20 10:18:14 +0800

    Update Slider.vue

--- src/components/Slider/Slider.vue
+++ src/components/Slider/Slider.vue
@@ -6,88 +6,96 @@
       </div>
     </div>
     <div v-if="mode === 'Slide' && sliderGroupRef" class="dots">
-      <div v-for="(dot, index) in dots" :key="index" :class="{active: currentPageIndex === index }"  class="dots-item"></div>
+      <div
+        v-for="(dot, index) in dots"
+        :key="index"
+        :class="{ active: currentPageIndex === index }"
+        class="dots-item"
+      ></div>
     </div>
   </div>
 </template>
 
 <script>
-import {onMounted, reactive, toRefs, shallowReactive} from "vue"
+import { onMounted, reactive, toRefs, shallowReactive } from "vue";
 
-import BScroll from "@better-scroll/core"
-import Slide from "@better-scroll/slide"
+import BScroll from "@better-scroll/core";
+import Slide from "@better-scroll/slide";
 
 const modeMap = new Map([
   ["Slide", () => BScroll.use(Slide)],
-  ["", new Function()]
-])
+  ["", new Function()],
+]);
 
 const initial = (state) => {
-  modeMap.get(state.mode)()
-  let slider =  new BScroll(state.sliderRef, state.sliderConf)
-  let hooks = slider.scroller.actionsHandler.hooks
-  
+  modeMap.get(state.mode)();
+  let slider = new BScroll(state.sliderRef, state.sliderConf);
+  let hooks = slider.scroller.actionsHandler.hooks;
+
   if (state.mode === "Slide") {
     slider.on("slideWillChange", (page) => {
-      state.currentPageIndex = page.pageX
-    })
+      state.currentPageIndex = page.pageX;
+    });
   }
 
   if (state.handleStart instanceof Function) {
-    hooks.on("start", event => {
-      state.handleStart(event)
-    })
+    hooks.on("start", (event) => {
+      state.handleStart(event);
+    });
   }
 
-  return slider
-}
+  return slider;
+};
 
 const setSliderWidth = (state) => {
-  let width = 0
-  let children = state.sliderGroupRef.children
-  let sliderWidth = state.sliderRef.clientWidth  
-  let _width = typeof state.sliderItemWidth === "number" ? state.sliderItemWidth : sliderWidth
-  let _height = state.sliderItemHeight
-  let needSetHeight = typeof _height === "number" && _height > 0
-  state.dots = new Array(...state.sliderGroupRef.children)
+  let width = 0;
+  let children = state.sliderGroupRef.children;
+  let sliderWidth = state.sliderRef.clientWidth;
+  let _width =
+    typeof state.sliderItemWidth === "number"
+      ? state.sliderItemWidth
+      : sliderWidth;
+  let _height = state.sliderItemHeight;
+  let needSetHeight = typeof _height === "number" && _height > 0;
+  state.dots = new Array(...state.sliderGroupRef.children);
   for (let i = 0, len = children.length; i < len; i++) {
-    let child = children[i]
-    child.classList.add("slider-item")
-    child.style.width = `${_width}px`
-    if (needSetHeight) child.style.height = `${_height}px`
-    width += _width
+    let child = children[i];
+    child.classList.add("slider-item");
+    child.style.width = `${_width}px`;
+    if (needSetHeight) child.style.height = `${_height}px`;
+    width += _width;
   }
-  state.sliderGroupRef.style.width = `${width}px`
-}
+  state.sliderGroupRef.style.width = `${width}px`;
+};
 
 const methods = () => {
-  return {initial, setSliderWidth}
-}
+  return { initial, setSliderWidth };
+};
 
 export default {
   props: {
     // BScroll实例配置
     sliderConf: {
       type: Object,
-      default: () => null
+      default: () => null,
     },
     // 模式: Slide(焦点轮播)
     mode: {
       type: String,
-      default: ""  
+      default: "",
     },
     sliderItemWidth: {
       type: Number,
-      defalut: null
+      defalut: null,
     },
     sliderItemHeight: {
       type: Number,
-      defalut: null
+      defalut: null,
     },
     handleStart: {
       type: Function,
-      default: null
-    }
+      default: null,
+    },
   },
   setup(props) {
     let state = reactive({
@@ -96,23 +104,23 @@ export default {
       currentPageIndex: 0,
       dots: [],
       ...props,
-      ...methods()
-    })
+      ...methods(),
+    });
 
     let state1 = shallowReactive({
-      slider: null
-    })
+      slider: null,
+    });
 
     onMounted(() => {
       setTimeout(() => {
-        state.setSliderWidth(state)
-        state1.slider = state.initial(state)
-      }, 200)
-    })
+        state.setSliderWidth(state);
+        state1.slider = state.initial(state);
+      }, 200);
+    });
 
-    return {...toRefs(state), ...state1}
-  }
-}
+    return { ...toRefs(state), ...state1 };
+  },
+};
 </script>
 
 <style lang="less">
@@ -149,16 +157,11 @@ export default {
       width: @dotsWidth;
       height: @dotsWidth;
       border-radius: 50%;
-      background-color: rgba(0,0,0,0.2);
+      background-color: rgba(0, 0, 0, 0.2);
       &.active {
-        background-color: rgba(255,58,58,1);
+        background-color: rgba(255, 58, 58, 1);
       }
     }
   }
 }
-
-    
-   
-      
-      
 </style>
