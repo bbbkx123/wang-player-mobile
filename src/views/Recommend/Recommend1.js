50222e874124e461e437a57476efac3b3fbbaf26
bbbkx <33077574+bbbkx123@users.noreply.github.com>
2020-10-22 17:33:33 +0800

    Create Recommend1.js

---
+++ src/views/Recommend/Recommend1.js
@@ -0,0 +1,109 @@
+
+import {
+  defineComponent,
+  ref,
+  unref,
+  reactive,
+  shallowReactive,
+  toRefs,
+  onBeforeMount,
+} from "vue";
+import { useRouter } from "vue-router";
+import Slider from "components/Slider";
+
+import { iconSlider, bannerConf, iconSliderConf } from "./define";
+import { ApiGetBanner, ApiGetTopPlayList, ApiGetPlayListTags } from "api";
+
+import "./index.less";
+
+export default defineComponent({
+  components: {
+    Slider,
+  },
+  setup() {
+    let router = useRouter();
+    // let _state = shallowReactive({
+    //   bannerConf,
+    //   iconSliderConf,
+    // });
+
+    // let state = reactive({
+    //   banners: [],
+    //   iconSlider,
+    //   iconSliderRef: null,
+    //   icon: [],
+    //   publicPath: process.env.BASE_URL,
+    //   handleClick,
+    // });
+    let bannerConf = ref(bannerConf),
+      iconSliderConf = ref(iconSliderConf),
+      banners = ref([]),
+      iconSlider = ref(iconSlider)
+
+    function getBanner() {
+      return ApiGetBanner({ type: 1 })
+        .then((res) => {
+          state.banners = res.data.banners;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+
+    function getTopPlayList() {
+      // ACG
+      return ApiGetTopPlayList(0, 6, "欧美")
+        .then((res) => {
+          state.iconSlider = res.data.playlists;
+        })
+        .catch((err) => {});
+    }
+
+    function getPlayListTags() {
+      return ApiGetPlayListTags()
+        .then((res) => {
+          debugger;
+        })
+        .catch((err) => {});
+    }
+
+    function handleClick(event) {
+      debugger;
+    }
+
+    // getPlayListTags(state)
+    getBanner();
+    // getTopPlayList(state)
+
+    onBeforeMount(() => {});
+
+    // return { ...toRefs(state), ...toRefs(_state) };
+    return () => (
+      <div class="recommend">
+        <Slider v-if="banners.length > 0" :sliderConf="bannerConf" mode="Slide">
+          <div v-for="(item, index) in banners" :key="index">
+            <a href="">
+              <img :src="item.imageUrl" alt="" />
+            </a>
+          </div>
+        </Slider>
+        <Slider
+          ref="iconSliderRef"
+          :sliderConf="iconSliderConf"
+          :sliderItemWidth="85"
+          :sliderItemHeight="85"
+          @hook-click="handleClick"
+        >
+          <div v-for="(item, index) in iconSlider" :key="index" class="icon-item">
+            <img
+              style="width: 80%; height: 80%"
+              :src="`${publicPath}image/${item.name}.png`"
+              alt=""
+            />
+          </div>
+        </Slider>
+        <div class="jingxuan">懂你的精选歌单</div>
+      </div>
+    )
+  },
+})
